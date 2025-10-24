0596d6fdd1e5d7edc95e5c07485c2482b7f56ba1
Carlos <cmatos3@ggc.edu>
2025-10-24 10:47:00 -0400

    trying to merge conflict

--- 01-read_input.sh
+++ 01-read_input.sh
@@ -11,6 +11,6 @@ echo 'Your name was stored in $name'
 # exercise: write a script that asks the user for a 
 # filename and create an empty file named after it
 
-echo "What would like to name your filename"
-read filename
-touch $filename.txt
+echo "NAME NAME NAME"
+read Read
+echo "merge conflict please $Read"
